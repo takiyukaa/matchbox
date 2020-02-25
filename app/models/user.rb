1cf68c152cc8cd43a7f0c8d95296c91f8f9e2eb0
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-25 16:05:35 +0900

    added cloudinary fertures to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :photo
   has_many :skills, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :work_histories, dependent: :destroy
