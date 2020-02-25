6f0ed306566c1dc02c66459693fb5ced46954fc5
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-02-25 15:08:28 +0900

    fixed user model confliction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   has_many :skills, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :work_histories, dependent: :destroy
 
   validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\S+@\S+.\S{2,3}/ }
-  validates :password, presence: true, length: { minimum: 8 }
-  validates :city, presence: true
-  validates :bio, presence: true
+  validates :second_name, presence: true
+  # validates :city, presence: true
+  # validates :bio, presence: true
 end
