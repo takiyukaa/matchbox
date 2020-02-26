aa9919982d559ccf967604b5fc4ce67666864e2c
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-26 13:00:59 +0900

    test user Ynn and Doug

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,85 @@ PROVIDERS = []
 BOOKERS = []
 PSKILLS = []
 
+START_TIME = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
+
+yann = User.create!(
+  email: "yann@gmail.com",
+  password: "123123",
+  first_name: "Yann",
+  second_name: "Klein",
+  city: "Tokyo",
+  bio: "Curiosity, creativity and people empowerment are the 3 things I value the most.
+I combined them to develop embedded systems in Asia, to lead software projects and innovation for a global company and to develop a VR startup in Canada.
+I use the best of these values today to help people reach their dream future at Le Wagon Kyoto."
+)
+
+css = Skill.create!(
+  name: "CSS",
+  description: "Cool for that",
+  user: yann
+)
+
+java = Skill.create!(
+  name: "Java Script",
+  description: "1. Select a element, 2. Listen to an event, 3. change DOM",
+  user: yann
+)
+
+WorkHistory.create!(
+    company_name: "Le wagon",
+    description: "the driver for part-time bootcamp.",
+    start_date: Date.today,
+    end_date: Date.today + rand(100..10000),
+    job_title: "Driver",
+    user: yann
+)
+
+doug = User.create!(
+  email: "doug@gmail.com",
+  password: "123123",
+  first_name: "Douglas",
+  second_name: "Berkley",
+  city: "Tokyo",
+  bio: "A web developer and director at Le Wagon Japan"
+)
+
+ruby = Skill.create!(
+  name: "Ruby",
+  description: "Flash card is super important",
+  user: doug
+)
+
+WorkHistory.create!(
+  company_name: "Le wagon",
+  description: "A web developer and director at Le Wagon Japan",
+  start_date: Date.today,
+  end_date: Date.today + rand(100..10000),
+  job_title: "Driver",
+  user: doug
+)
+
+Booking.create!(
+  date: Date.today - rand(1..10),
+  start_time: START_TIME,
+  end_time: START_TIME + rand(1..2).hours,
+  status: ['accepted', 'rejected', 'pending'].sample,
+  skill: ruby,
+  user: yann,
+  user_comment: Faker::TvShows::Simpsons.quote,
+  provider_comment: Faker::TvShows::Simpsons.quote
+)
+
+Booking.create!(
+  date: Date.today - rand(1..10),
+  start_time: START_TIME,
+  end_time: START_TIME + rand(1..2).hours,
+  status: ['accepted', 'rejected', 'pending'].sample,
+  skill: css,
+  user: doug,
+  user_comment: Faker::TvShows::Simpsons.quote,
+  provider_comment: Faker::TvShows::Simpsons.quote
+)
 
 
 
@@ -54,7 +133,6 @@ end
     second_name: Faker::Name.last_name,
     city: Faker::Address.city,
     bio: Faker::Hipster.paragraphs
-    photo:
     )
   BOOKERS << user
 
@@ -76,11 +154,10 @@ end
 end
 
 3.times do
-  start_time = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)
   Booking.create!(
     date: Date.today - rand(1..10),
-    start_time: start_time,
-    end_time: start_time + rand(1..2).hours,
+    start_time: START_TIME,
+    end_time: START_TIME + rand(1..2).hours,
     status: ['accepted', 'rejected', 'pending'].sample,
     skill: PSKILLS.sample,
     user: BOOKERS.sample,
