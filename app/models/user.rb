b7080d4b893d826d6ac896ec8e70f0f577a6af82
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-26 10:17:26 +0900

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :skills, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :bokkings_as_owner, through: :skills, source: :bookings
+  has_many :bookings_as_owner, through: :skills, source: :bookings
   has_many :work_histories, dependent: :destroy
 
   validates :first_name, presence: true
