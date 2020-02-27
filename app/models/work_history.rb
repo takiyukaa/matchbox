9961322b4d15a61c3ef01874ef37c6db45cc5681
jethro tang <jthtang10@outlook.com>
2020-02-27 15:49:26 +0900

    work history controller and view

--- app/models/work_history.rb
+++ app/models/work_history.rb
@@ -1,3 +1,8 @@
 class WorkHistory < ApplicationRecord
   belongs_to :user
+  validates :job_title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_date, presence: true
+  validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :bookings, only: [:index]
-  
+  resources :work_histories, only: [:new, :create]
   resources :skills, only: [:index, :show, :new, :create] do
     collection do
       get 'search'
