afc5aea391045e54d56bf0000d336961768af73d
Yuka Takiyama <yukatakiyama27@gmail.com>
2020-02-27 15:24:25 +0900

    user can make a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,10 +6,31 @@ class BookingsController < ApplicationController
   end
 
   def new
-
+    @booking = Booking.new
+    @user = User.find(params[:user_id])
+    authorize @booking
   end
+
 
   def create
-
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+      authorize @booking
+    else
+      render :new
+    end
   end
+
+  def booking_params
+
+    params.require(:booking)
+          .permit(:date,
+                  :start_time,
+                  :end_time,
+                  :user_comment,
+                  :skill_id)
+  end
+
 end
