830cced012e4634d4273b92a36abca4fcfbc6d41
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-02-28 13:27:11 +0900

    Added bookings show view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,14 @@
 import "bootstrap";
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+
+initSweetalert('#cancel-booking', {
+  title: "Cancel Booking",
+  text: "Are you sure?",
+  icon: "warning"
+}, (value) => {
+  if (value) {
+    var cancelButton = document.querySelector('#cancel-booking-confirmed');
+    cancelButton.click();
+}});
